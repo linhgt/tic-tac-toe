35bd06f2ab8ee6876e47f0b0a30373c01de9b9f3
Linh Nguyen <linhnhatmen@gmail.com>
2022-11-07 16:29:26 +0700

    Initialize

---
+++ src/components/mark-choice/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { MARK } from '../../constant.js';
+import Switch from './components/switch';
+import styles from './choose-mark.module.scss';
+
+const MarkChoice = (props) => {
+    const [mark, setMark] = useState(MARK.X);
+    const listChoices = [
+        {
+            name: "cross",
+            value: MARK.X
+        },
+        {
+            name: "nought",
+            value: MARK.O
+        }
+    ]
+    
+    const setChoice = (value) => {
+        setMark(value);
+    }
+
+    return (
+        <div class={styles.wrapper}>
+            <Switch listChoices={listChoices} choice={mark} setChoice={setChoice}/>
+        </div>
+    )
+}
