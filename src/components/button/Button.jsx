d5bb65b53e84a20967e976375e21b4a2059f7efa
Linh Nguyen <linhnhatmen@gmail.com>
2022-11-07 16:28:06 +0700

    Initialize

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import cx from 'classnames';
+import style from './button.module.scss';
+
+const Button = (props) => {
+    const {className, children, ...others} = props;
+
+    return (
+        <button className={cx(className, 'common-item', style.button)}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
