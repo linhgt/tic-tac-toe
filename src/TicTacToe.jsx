c916f465dc1f1d7d7f9442b6d30e7220a50f5d67
linh <linhgt65@gmail.com>
2023-01-09 10:35:43 +0700

    reconfigure projects

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Button from "./components/button/Button.jsx";
+// import X_ICON from "./assets/images/ic_blue_x.svg";
+// import O_ICON from "./assets/images/ic_yellow_o.svg";
 import styles from './main-menu.module.scss';
 
 const TicTacToe = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.gameMenu}>
-        <h1>Welcome to Tic Tac Toe</h1>
-        <Button>
-          Hello
-        </Button>
-        <Button>
-          How are you
-        </Button> 
+    <div className={styles['container']}>
+      <div className={styles['game-menu']}>
+        <div className={styles['game-menu__header']}>
+          <img src="/assets/images/ic_blue_x.svg" alt="X"/>
+          <img src="/assets/images/ic_yellow_o.svg" alt="O"/>
+        </div>
       </div>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js")},
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+        options : { presets: ["@babel/preset-env", "@babel/preset-react"] },
       },
       {
         test: /\.css$/,
@@ -19,21 +19,26 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.svg$/,
+        use: ["url-loader"]
       }
-    ]
+    ],
   },
   output: {
-    path: path.resolve(__dirname, "/dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
   },
   devServer: {
     port: 3000,
-    static: './dist',
+    static: '/dist',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
-      template: './public/index.html'
+      title: 'tic-tac-toe',
+      template: 'src/template.html'
     })
   ]
 };
