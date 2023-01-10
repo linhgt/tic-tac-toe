9270a7a136bddaaa17508c71007bf15cf08ad31f
linh <linhnhatmen@gmail.com>
2023-01-10 15:13:45 +0700

    refactor structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader";
-import TicTacToe from "./TicTacToe.jsx";
+// import TicTacToe from "./containers/TicTacToe.jsx";
+import MainMenu from "@containers/main-menu";
 import "./app.scss";
 
 class App extends Component{
     render(){
         return (
-            <TicTacToe />
+            <MainMenu />
         )
     }
 }
--- src/containers/TicTacToe.jsx
+++ src/containers/TicTacToe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Button from "./components/button/Button.jsx";
-import X_ICON from "./assets/images/ic_blue_x.svg";
-import O_ICON from "./assets/images/ic_yellow_o.svg";
+import Button from "@components/button/";
+import X_ICON from "@assets/images/ic_blue_x.svg";
+import O_ICON from "@assets/images/ic_yellow_o.svg";
 import styles from './main-menu.module.scss';
 
 const TicTacToe = () => {
@@ -11,6 +11,12 @@ const TicTacToe = () => {
         <div className={styles['game-menu__header']}>
           <img src={X_ICON} alt="X"/>
           <img src={O_ICON} alt="O"/>
+        </div>
+        <div className={styles['game-menu__marks-card']}>
+          <h5>Pick player 1's mark</h5>
+          <div className={styles['marks']}>
+            
+          </div>
         </div>
       </div>
     </div>
