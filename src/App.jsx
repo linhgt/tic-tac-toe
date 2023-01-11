3d504b96d4cf60002c7fac26bdeb04ef765d67d1
linh <linhnhatmen@gmail.com>
2023-01-11 18:55:49 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { RouterProvider } from "react-router-dom";
-import router from './router'
+import router from "./router";
 // import TicTacToe from "./containers/TicTacToe.jsx";
 import "./app.scss";
 
 class App extends Component {
   render() {
-    return <RouterProvider router={router} fallbackElement={<div>loading..</div>}/>
+    return (
+      <RouterProvider router={router} fallbackElement={<div>loading..</div>} />
+    );
   }
 }
 
