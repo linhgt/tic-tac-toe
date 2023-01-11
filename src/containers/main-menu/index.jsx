0bb232a372d8422a6a868d7bb810ddb85d0f1810
linh <linhnhatmen@gmail.com>
2023-01-11 18:54:16 +0700

    update main menu

--- src/containers/main-menu/index.jsx
+++ src/containers/main-menu/index.jsx
@@ -1,32 +1,52 @@
-import React, { useMemo, useState } from "react";
-// import X_ICON from "@assets/images/ic_blue_x.svg";
-// import O_ICON from "@assets/images/ic_yellow_o.svg";
-import { X, O } from "@assets/images/Icons";
-import Switch from "@components/switch";
+import React, { useCallback, useMemo, useState } from "react";
 import { MARK } from "../../constant";
+import ICONS from "@assets/images/Icons";
+import Switch from "@components/switch";
 import styles from "./main-menu.module.scss";
 
 const MainMenu = () => {
-  const [selectedMark, setSelectMark] = useState();
-  const listChoice = useMemo(() => [
-    {
-      id: 1,
-      value: 1,
-      name: MARK.X,
-      content: <img src="" alt="X"/>
-    }
-  ], [])
-  
+  const choices = useMemo(
+    () => ({
+      type: {
+        [MARK.X]: {
+          id: 1,
+          value: 1,
+          name: MARK.X,
+          content: () => <ICONS.X  />
+        },
+        [MARK.O]: {
+          id: 2,
+          value: 2,
+          name: MARK.O,
+          content: () => <ICONS.O />
+        }
+      },
+      list: [MARK.X, MARK.O]
+    }),
+    []
+  );
+  const [selectedMark, setSelectMark] = useState(MARK.X);
+
+  const _onSelectChoices = useCallback((choice) => {
+    setSelectMark(choice);
+  }, []);
+
   return (
     <div className={styles["container"]}>
       <div className={styles["game-menu"]}>
         <div className={styles["game-menu__header"]}>
-          <X />
-          <O />
+          <ICONS.X />
+          <ICONS.O />
         </div>
         <div className={styles["game-menu__marks-card"]}>
           <h5>Pick player 1's mark</h5>
-          <div className={styles["marks"]}></div>
+          <div className={styles["marks"]}>
+            <Switch
+              listChoice={choices}
+              selectedChoice={selectedMark}
+              _onSelectChoices={_onSelectChoices}
+            />
+          </div>
         </div>
       </div>
     </div>
