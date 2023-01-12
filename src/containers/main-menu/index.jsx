98d160ae4d1e28c467dcb8afd8deb6a4825e76d1
linh <linhnhatmen@gmail.com>
2023-01-12 12:44:30 +0700

    update main menu

--- src/containers/main-menu/index.jsx
+++ src/containers/main-menu/index.jsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useMemo, useState } from "react";
+import { Link } from 'react-router-dom';
 import { MARK } from "../../constant";
 import ICONS from "@assets/images/Icons";
 import Switch from "@components/switch";
 import styles from "./main-menu.module.scss";
+import Button from '@components/button';
 
 const MainMenu = () => {
   const choices = useMemo(
@@ -12,7 +14,7 @@ const MainMenu = () => {
           id: 1,
           value: 1,
           name: MARK.X,
-          content: () => <ICONS.X  />
+          content: () => <ICONS.X />
         },
         [MARK.O]: {
           id: 2,
@@ -39,14 +41,25 @@ const MainMenu = () => {
           <ICONS.O />
         </div>
         <div className={styles["game-menu__marks-card"]}>
-          <h5>Pick player 1's mark</h5>
-          <div className={styles["marks"]}>
-            <Switch
-              listChoice={choices}
-              selectedChoice={selectedMark}
-              _onSelectChoices={_onSelectChoices}
-            />
-          </div>
+          <h1>Pick player 1's mark</h1>
+          <Switch
+            listChoice={choices}
+            selectedChoice={selectedMark}
+            _onSelectChoices={_onSelectChoices}
+          />
+          <p>REMEMBER: X GOES FIRST</p>
+        </div>
+        <div className={styles['game-menu__btns']}>
+          <Link style={{height: "67px"}}>          
+            <Button type="button" className={styles["vs_cpu"]}>
+              NEW GAME (VS CPU)
+            </Button>
+          </Link>
+          <Link style={{height: "67px"}}>   
+            <Button type="button" className={styles["vs_player"]}>
+              NEW GAME (VS PLAYER)
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
