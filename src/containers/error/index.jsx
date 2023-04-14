0041a84c770d563de5258f2edfaaf4b76865215e
LinhNN <linhnn@vmodev.com>
2023-04-14 16:48:47 +0700

    Add error handling

---
+++ src/containers/error/index.jsx
@@ -0,0 +1,10 @@
+import { useRouteError } from "react-router";
+
+function ErrorComponent() {
+  const { error } = useRouteError();
+  console.error(error);
+
+  return <h1>An error occurs...</h1>;
+}
+
+export default ErrorComponent;
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorComponent from "@/containers/error";
 import MainMenu from "@/containers/main-menu/";
 import GameBoard from "@/containers/game-board";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainMenu />
-  },
-  {
-    path: "/game-board",
-    element: <GameBoard />
+    errorElement: (
+      <ErrorComponent />
+    ) /** Handle Error in the root level, specifying custom error component to display error */,
+    children: [
+      {
+        path: "",
+        element: <MainMenu />
+      },
+      {
+        path: "/game-board",
+        element: <GameBoard />
+      }
+    ]
   }
 ]);
 
